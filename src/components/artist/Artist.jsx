6c9e64dd0dbc02f861cb8f252ffe1a4441b82919
Bahattin Berkdinc <bberkdinc@gmail.com>
2023-08-16 00:58:45 +0300

    artist page finished

--- src/components/artist/Artist.jsx
+++ src/components/artist/Artist.jsx
@@ -1,66 +1,68 @@
+import { useEffect, useState } from "react";
+import "./artist.scss";
+import { Col, Container, Row } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
+import axios from "axios";
 
-import { useContext, useEffect, useState } from "react"
-import "./artist.scss"
-import { Col, Container, Row } from 'react-bootstrap'
-import { useParams } from 'react-router-dom'
-import axios from "axios"
-import StoreContext from "../../store"
 const Artist = () => {
 
-    const store = useContext(StoreContext)
+    const { castId } = useParams();
+    console.log(castId, "castId");
 
-    const {castId} = useParams()
-    console.log(castId , "castId");
+    const [artistDetail, setArtistDetail] = useState({});
 
-
-    const [artistDetail, setArtistDetail]=useState([])
- 
-    const fetchMovieDetails = async (castId) => {
+    const fetchArtistDetails = async (castId) => {
         try {
-          const response = await axios.get(`https://api.themoviedb.org/3/movie/${castId}`, {
-            params: {
-              api_key: '0e8368c16fab615ffc56aec01936d0c2',
-              append_to_response: 'credits,keywords',
-            }
-          });
-      
-          setArtistDetail(response.data.credits.cast);
-          
+            const response = await axios.get(`https://api.themoviedb.org/3/person/${castId}`, {
+                params: {
+                    api_key: '0e8368c16fab615ffc56aec01936d0c2',
+                    append_to_response: 'movie_credits',
+                }
+            });
+
+            setArtistDetail(response.data);
         } catch (error) {
-          console.log(error);
-          return null;
+            console.log(error);
+            return null;
         }
-      };
+    };
 
-      
-      useEffect(() => {
+    useEffect(() => {
         window.scrollTo(0, 0);
-        fetchMovieDetails(castId);
-      }, [castId]);
-    
+        fetchArtistDetails(castId);
+    }, [castId]);
 
-      console.log(artistDetail , "artistDetail");
-   
+    console.log(artistDetail, "artistDetail");
 
-     
-      
-      
-     
-
-  return (
-    <div className='artist-bio'>
-      <Container>
-        <Row>
-            <Col>
-            <img src="" style={{width:"100%"}} alt="" />
-            </Col>
-            <Col>
-            
-            </Col>
-        </Row>
-      </Container>
-    </div>
-  )
+    return (
+        <div className='artist-bio'>
+            <Container>
+                <Row>
+                    <Col md={3}>
+                        <img src={`https://image.tmdb.org/t/p/original${artistDetail?.profile_path}`} style={{ width: "100%" }} alt="" />
+                    </Col>
+                    <Col md={9}>
+                        <div className="actor-info">
+                        <p className="text-white">Name : <span>{artistDetail?.name}</span></p>
+                        <p className="text-white">Place of Birth : <span>{artistDetail?.place_of_birth}</span></p>
+                        <p className="text-white">Name : <span>{artistDetail?.biography}</span></p>
+                        <div className="other-movies">
+                          <Row>
+                            {
+                              artistDetail?.movie_credits?.cast.splice(0,12).map((cast, index) => (
+                                <Col sm={2} key={index} className="gy-4">
+                                <img src={`https://image.tmdb.org/t/p/original${cast.poster_path}`} alt="" />
+                              </Col> 
+                              ))
+                            }
+                          </Row>
+                        </div>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
 }
 
-export default Artist
+export default Artist;
--- src/components/movie-detail/MovieDetail.jsx
+++ src/components/movie-detail/MovieDetail.jsx
@@ -1,146 +1,122 @@
-
 import { useContext, useEffect, useState } from 'react';
 import StoreContext from '../../store';
 import { Col, Container, Row } from 'react-bootstrap';
 import { AiOutlineStar } from 'react-icons/ai';
-import {CgPlayTrackNextO} from 'react-icons/cg'
+import { CgPlayTrackNextO } from 'react-icons/cg';
 import { Link, NavLink, useParams } from 'react-router-dom';
 import "./moviedetailcomp.scss"
 import axios from 'axios';
 import Header from '../header/Header';
-const MovieDetail = ({onGenresChange}) => {
+
+const MovieDetail = ({ onGenresChange }) => {
 
     const store = useContext(StoreContext);
-    const { id } = useParams(); 
-  
-    const selectedMovie = store.find((movie) => movie.id.toString() === id); 
+    const { id } = useParams();
 
-    const [movieDetail, setMovieDetail]=useState([])
+    const selectedMovie = store.find((movie) => movie.id.toString() === id);
+
+    const [movieDetail, setMovieDetail] = useState([]);
     const [genreIds, setGenreIds] = useState([]);
- 
+
     const fetchMovieDetails = async (id) => {
         try {
-          const response = await axios.get(`https://api.themoviedb.org/3/movie/${id}`, {
-            params: {
-              api_key: '0e8368c16fab615ffc56aec01936d0c2',
-              append_to_response: 'credits,keywords',
-            }
-          });
-      
-          setMovieDetail(response.data);
+            const response = await axios.get(`https://api.themoviedb.org/3/movie/${id}`, {
+                params: {
+                    api_key: '0e8368c16fab615ffc56aec01936d0c2',
+                    append_to_response: 'credits,keywords',
+                }
+            });
 
-        
-          const genreIds = response.data.genres ? response.data.genres.map((genre) => genre.id) : [];
-          setGenreIds(genreIds);
-    
-         
-          onGenresChange(genreIds);
-          
+            setMovieDetail(response.data);
+
+            const genreIds = response.data.genres ? response.data.genres.map((genre) => genre.id) : [];
+            setGenreIds(genreIds);
+
+            onGenresChange(genreIds);
+
         } catch (error) {
-          console.log(error);
-          return null;
+            console.log(error);
+            return null;
         }
-      };
+    };
 
-      
-      useEffect(() => {
+    useEffect(() => {
         window.scrollTo(0, 0);
         fetchMovieDetails(id);
-      }, [id]);
+    }, [id]);
 
-      if (!selectedMovie) {
-        return null; 
-      }
-    
-      
-   
+    if (!selectedMovie) {
+        return null;
+    }
 
     const genre = movieDetail.genres ? movieDetail.genres.map((genre) => genre.name) : [];
-    // const genreIds = movieDetail.genres ? movieDetail.genres.map((genre) => genre.id) : [];
-    //  const cast = movieDetail?.credits?.cast ? movieDetail.credits.cast.map((cast) => cast.name)  : [];
-
-    //  const castId = movieDetail?.credits?.cast ? movieDetail.credits.cast.splice(0,5).map((cast) => cast.id)  : [];
-    //  console.log(castId);
-
     const castList = movieDetail?.credits?.cast || [];
-    const castId = castList.slice(0, 5).map((cast) => cast.id);
-    console.log(castId + "castId-movieDetail");
+    const castProfilePic = movieDetail?.credits?.cast ? movieDetail.credits.cast.map((cast) => cast.profile_path) : [];
+    const director = movieDetail?.credits?.crew ? movieDetail.credits.crew
+        .filter((crew) => crew.known_for_department === 'Directing')
+        .map((crew) => crew.name)
+        .join(', ') : '';
 
-     const castProfilePic = movieDetail?.credits?.cast ? movieDetail.credits.cast.map((cast) => cast.profile_path)  : [];
-
-     const director = movieDetail?.credits?.crew ? movieDetail.credits.crew.map((crew) =>{
-       if(crew.known_for_department === 'Directing'){
-         return crew.name
-        }else{
-          return null
-        }
-      })  : [];
-      
-    
-
-  return (
-    <div className="movie-detail" style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${selectedMovie.poster_path})`}}>
-        <Header/>
-        <Container>
-        <div className="movie-detail-top">
-          <h2>{selectedMovie.title} <span>({selectedMovie.release_date})</span></h2>
-          <span>
-            {
-              Array(10).fill().map((_, i) => (
-                <AiOutlineStar key={i} className={i < selectedMovie.vote_average ? 'star filled' : 'star empty'} />
-              ))
-            }
-          </span>
-        </div>
-        <div className="movie-detail-bottom">
-          <Row>
-            <Col sm={12} md={4} >
-            <div className="movie-img">
-            <img src={`https://image.tmdb.org/t/p/original${selectedMovie.poster_path}`} alt={selectedMovie.title} />
-            </div>
-            </Col>
-            <Col sm={12} md={8} >
-            <div className="movie-info">
-              <div className="info-top">
-                <div className="info-top-left">
-                  <p>Release : <span>{selectedMovie.release_date}</span></p>
-                  <p>Directing : <span>{director.splice(0,1) }</span></p>
-                  <p>Genres : <span>{genre.splice(0, 3).join(', ')}</span></p>
+    return (
+        <div className="movie-detail" style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original${selectedMovie.poster_path})` }}>
+            <Header />
+            <Container>
+                <div className="movie-detail-top">
+                    <h2>{selectedMovie.title} <span>({selectedMovie.release_date})</span></h2>
+                    <span>
+                        {Array(10).fill().map((_, i) => (
+                            <AiOutlineStar key={i} className={i < selectedMovie.vote_average ? 'star filled' : 'star empty'} />
+                        ))}
+                    </span>
                 </div>
-                <div className="info-top-right">
-                  
-                  <p>Runtime : <span>{movieDetail.runtime}</span> minute</p>
-                  <span className='play-trailer'><CgPlayTrackNextO/>Play Trailer </span>
+                <div className="movie-detail-bottom">
+                    <Row>
+                        <Col sm={12} md={4} >
+                            <div className="movie-img">
+                                <img src={`https://image.tmdb.org/t/p/original${selectedMovie.poster_path}`} alt={selectedMovie.title} />
+                            </div>
+                        </Col>
+                        <Col sm={12} md={8} >
+                            <div className="movie-info">
+                                <div className="info-top">
+                                    <div className="info-top-left">
+                                        <p>Release : <span>{selectedMovie.release_date}</span></p>
+                                        <p>Directing : <span>{director}</span></p>
+                                        <p>Genres : <span>{genre.slice(0, 3).join(', ')}</span></p>
+                                    </div>
+                                    <div className="info-top-right">
+                                        <p>Runtime : <span>{movieDetail.runtime}</span> minute</p>
+                                        <span className='play-trailer'><CgPlayTrackNextO />Play Trailer </span>
+                                    </div>
+                                </div>
+                                <div className="cast">
+                                    <p>
+                                        Cast: {castList.slice(0, 5).map((cast, index) => (
+                                            <NavLink key={index} as={Link} to={`/cast/${cast.id}`}>
+                                                {cast.name}
+                                                {index < 4 ? ', ' : ''}
+                                            </NavLink>
+                                        ))}
+                                    </p>
+                                    <div className="cast-profile">
+                                        {castProfilePic.slice(0, 5).map((pic, index) => (
+                                            <div className='profile-pic-box' key={index}>
+                                                <img key={index} src={`https://image.tmdb.org/t/p/original${pic}`} alt={pic} />
+                                            </div>
+                                        ))}
+                                    </div>
+                                </div>
+                                <div className="info-bottom">
+                                    <h2>{selectedMovie.title}</h2>
+                                    <p>{selectedMovie.overview}</p>
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
                 </div>
-              </div>
-              <div className="cast">
-              <p>
-            Cast: {castList.slice(0, 5).map((cast, index) => (
-              <NavLink key={index} as={Link} to={`/cast/${id}`}>
-             {cast.name}
-               {index < 4 ? ', ' : ''}
-              </NavLink>
-               ))}
-                </p>
-
-                <div className="cast-profile">{castProfilePic.splice(0, 5).map((pic, index) => (
-                  <div className='profile-pic-box' key={index}>
-                    <img key={index} src={`https://image.tmdb.org/t/p/original${pic}`} alt={pic} />
-                  </div>
-                ))}</div>
-              </div>
-              <div className="info-bottom">
-              <h2>{selectedMovie.title}</h2>
-            <p>{selectedMovie.overview}</p>
-              </div>
-            </div>
-            </Col>
-          </Row>
+            </Container>
         </div>
-          
-    </Container>
-        </div>
-  )
+    )
 }
 
-export default MovieDetail
+export default MovieDetail;
